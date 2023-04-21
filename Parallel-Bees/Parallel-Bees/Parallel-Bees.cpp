19042497f3e6cdfe46923a8a8f49989c415351cb
Olivera Radovanović <90214731+Olivera2708@users.noreply.github.com>
2023-04-21 22:08:28 +0200

    added json

--- Parallel-Bees/Parallel-Bees/Parallel-Bees.cpp
+++ Parallel-Bees/Parallel-Bees/Parallel-Bees.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 int main()
 {
-    cout << "Hello World!\n";
+    // TEST ZA JSON
+    // 
+    // 
+    // JSON string to parse
+    std::string json_string = "{\"name\": \"John\", \"age\": 30, \"isStudent\": true, \"grades\": [8.5, 9, 7.5]}";
+
+    // Parse the JSON string
+    json data = json::parse(json_string);
+
+    // Access the JSON data as a C++ object
+    std::string name = data["name"];
+    int age = data["age"];
+    bool is_student = data["isStudent"];
+    std::vector<double> grades = data["grades"];
+
+    // Print the parsed data
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Is student: " << is_student << std::endl;
+    std::cout << "Grades: ";
+    for (auto grade : grades) {
+        std::cout << grade << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
